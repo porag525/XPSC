0e56136f3f136db502128eadb015e05cc8f22e62
Md.Moniruzzaman Porag <121655147+porag525@users.noreply.github.com>
2023-06-20 22:42:13 +0600

    Update F_3_SUM.cpp

--- Week 3/Day 5/F_3_SUM.cpp
+++ Week 3/Day 5/F_3_SUM.cpp
@@ -42,34 +42,3 @@ int main()
         solve();
     }
 }
-
-/*
-Ashik Bin Aziz
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-   int t; cin>>t; while(t--)
-   {
-       int n;       cin>>n;
-       vector<int>v;
-       map<int,int>mp;
-       while(n--)
-       {
-           int x;   cin>>x;
-           int y=x%10;  mp[y]++;
-           if(mp[y]<=3) v.push_back(y);
-       }
-       bool ok=false;
-       for(int i=0; i<v.size(); i++)
-        for(int j=i+1; j<v.size(); j++)
-         for(int k=j+1; k<v.size(); k++)
-         {
-            int sum=v[i]+v[j]+v[k];
-            if(sum%10==3) ok=true;
-         }
-
-         if(ok) cout<<"YES\n";
-         else cout<<"NO\n";
-   }
-}*/
