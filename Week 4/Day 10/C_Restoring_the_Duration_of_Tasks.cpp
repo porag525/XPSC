056c91893d7c841b4180973bc6d461c71b0c0359
porag525 <moniruzzaman1510@gmail.com>
2023-07-05 23:54:31 +0600

    5th problem

--- Week 4/Day 10/C_Restoring_the_Duration_of_Tasks.cpp
+++ Week 4/Day 10/C_Restoring_the_Duration_of_Tasks.cpp
@@ -2,6 +2,38 @@
 using namespace std;
 void solve()
 {
+  int n;cin>>n;
+  int a[n],b[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>a[i];
+  }
+  for(int i=0;i<n;i++)
+  {
+    cin>>b[i];
+  }
+  vector<int>ans;
+  int x=b[0]-a[0];
+  ans.push_back(x);
+  for(int i=1;i<n;i++)
+  {
+    if(a[i]<b[i-1])
+    {
+      int y=b[i]-b[i-1];
+      ans.push_back(y);
+    }
+    else 
+    {
+       int x=b[i]-a[i];
+       ans.push_back(x);
+    }
+
+  }
+  for(auto u:ans)
+  {
+    cout<<u<<" ";
+  }
+  cout<<endl;
    
 }
 int main()
