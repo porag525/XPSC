b7dd7636c582c1f3cdf81432fe90449255037be5
porag525 <moniruzzaman1510@gmail.com>
2023-08-18 20:00:36 +0600

    Update

---
+++ Week 11/Day 1/A_Mocha_and_Math.cpp
@@ -0,0 +1,39 @@
+/*
+__________________________
+| Author:Md.Moniruzzaman |
+--------------------------
+*/
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define f front
+using namespace std;
+void solve()
+{
+    int n;cin>>n;
+    int x[n];
+    for(int i=0;i<n;i++) cin>>x[i];
+   int ans=x[0];
+   for(int i=1;i<n;i++)
+   {
+     ans=(ans & x[i]); 
+   }
+    cout<<ans<<endl;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tst;
+    cin >> tst;
+    while (tst--)
+    {
+        solve();
+    }
+return 0;
+}
---
+++ Week 11/Day 1/B_AND_0_Sum_Big.cpp
@@ -0,0 +1,35 @@
+/*
+__________________________
+| Author:Md.Moniruzzaman |
+--------------------------
+*/
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define f front
+using namespace std;
+ll mod=1e9 +7;
+void solve()
+{
+    int n,k;cin>>n>>k;
+    ll ans=1;
+    for(int i=0;i<k;i++) ans=(ans*n)%mod;
+    cout<<ans<<endl;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tst;
+    cin >> tst;
+    while (tst--)
+    {
+        solve();
+    }
+return 0;
+}
